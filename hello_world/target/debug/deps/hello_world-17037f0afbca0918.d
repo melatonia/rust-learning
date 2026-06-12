bab501dc1653c4e4f9b874a60e566e19f71a233a
melatonia <alperenn.onderr@gmail.com>
2026-06-12 15:35:05 +0300

    fix: untrack target dir and compiled binary

--- hello_world/target/debug/deps/hello_world-17037f0afbca0918.d
+++
@@ -1,9 +0,0 @@
-/home/melatonia/Projects/dev/rust/rust-learning/hello_world/target/debug/deps/hello_world-17037f0afbca0918.d: src/main.rs Cargo.toml
-
-/home/melatonia/Projects/dev/rust/rust-learning/hello_world/target/debug/deps/libhello_world-17037f0afbca0918.rmeta: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
