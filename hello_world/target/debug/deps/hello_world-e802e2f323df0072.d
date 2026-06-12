f0dffbd1feb4fdc5065999f774e15bf9de76a89d
melatonia <alperenn.onderr@gmail.com>
2026-06-12 15:33:41 +0300

    add: hello_world project

---
+++ hello_world/target/debug/deps/hello_world-e802e2f323df0072.d
@@ -0,0 +1,9 @@
+/home/melatonia/Projects/dev/rust/rust-learning/hello_world/target/debug/deps/hello_world-e802e2f323df0072.d: src/main.rs Cargo.toml
+
+/home/melatonia/Projects/dev/rust/rust-learning/hello_world/target/debug/deps/libhello_world-e802e2f323df0072.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
